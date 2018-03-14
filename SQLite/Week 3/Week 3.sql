d828c0a4395e28eac27dede606d8d2ee9db0bdc4
Vishal Kumar <vishalks1993@gmail.com>
2018-03-14 22:18:15 +0530

    Update Week 3.sql

--- SQLite/Week 3/Week 3.sql
+++ SQLite/Week 3/Week 3.sql
@@ -1,6 +1,6 @@
 SELECT CustomerID, CompanyName, Region
 FROM Customers
-WHERE cuntomerID in 
+WHERE customerID in 
 	(SELECT customerID
 	FROM Orders
 	WHERE Freight > 100);
@@ -11,14 +11,24 @@ WHERE cust_id IN
 	SELECT customer_id
 	FROM Orders
 	WHERE order_number IN (SELECT order_number
-							FROM OrderItems
-							WHERE prod_name = 'Toothbrush');
+				FROM OrderItems
+				WHERE prod_name = 'Toothbrush');
 
 SELECT customer_name, customer_state,
-								(SELECT COUNT (*) AS orders
-								FROM Orders
-								WHERE Orders.customer_id = Customer.customer_id)
-								as orders
+		(SELECT COUNT (*) AS orders
+		FROM Orders
+		WHERE Orders.customer_id = Customer.customer_id)
+		as orders
 FROM customers
 ORDER BY Customer_name;
 					
+SELECT product_name, unit_price, company_name
+FROM suppliers CROSS JOIN products;
+
+SELECT suppliers.CompanyName, ProductName, UnitPrice
+FROM Suppliers INNER JOIN Products 
+ON Suppliers.supplierid = Products.supplierid;
+
+SELECT o.OrderID, c.CompanyName, e.LastName
+FROM ((Orders o INNER JOIN Customers c ON o.CustomerID = c.CustomerID) 
+		INNER JOIN Employees e ON o.EmployeeID = e.EmployeeID);
